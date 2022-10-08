67745e6ae05be81a58452b13d79bcdfc8c84eaba
Rafo-1999 <mn.raf.99@mail.ru>
2022-10-08 23:03:48 +0400

    .

---
+++ src/main/java/lab/BankAccount.java
@@ -0,0 +1,13 @@
+package lab;
+
+public class BankAccount {
+    double balance;
+  public void account(){
+     balance = 0.0;
+      }
+
+      public  void addMoney(double money){
+        balance=balance+money;
+      }
+
+}
